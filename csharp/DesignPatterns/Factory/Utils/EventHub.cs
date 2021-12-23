b58e8d864c98a6d51efd8c28357769773add4655
pawel.wilkosz@gmail.com <pawel.wilkosz@gmail.com>
2021-12-23 12:30:33 +0100

    Factory design pattern

---
+++ csharp/DesignPatterns/Factory/Utils/EventHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Utils
+{
+    public class EventHub : MessageProcessor
+    {
+        public string EventHubNamespace { get; }
+        public string EventHubName { get; }
+        public EventHub(string eventHubNamespace, string eventHubName)
+        {
+            EventHubNamespace = eventHubNamespace;
+            EventHubName = eventHubName;
+        }
+    }
+}

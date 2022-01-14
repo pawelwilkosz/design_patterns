1620d3ed26aaf7b423919ccd6e4b970a87a1bdb8
pawel.wilkosz@motorolasolutions.com <pawel.wilkosz@motorolasolutions.com>
2022-01-14 17:04:17 +0100

    Singleton implementation

---
+++ csharp/DesignPatterns/Singleton/Singleton/DatabaseConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class DatabaseConnector
+    {
+        public static DatabaseConnector Instance => instance.Value;
+
+        private DatabaseConnector()
+        {
+            Console.WriteLine("Database object initialization...");
+        }
+
+        private static Lazy<DatabaseConnector> instance =
+            new Lazy<DatabaseConnector>(
+                () => new DatabaseConnector()
+                );
+    }
+}
---
+++ csharp/DesignPatterns/Singleton/Singleton/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            DatabaseConnector dbA = DatabaseConnector.Instance;
+            DatabaseConnector dbB = DatabaseConnector.Instance;
+
+            Console.WriteLine(dbA == dbB);
+         }
+    }
+}
